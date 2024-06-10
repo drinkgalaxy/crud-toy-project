091ed3ddd1d1b82963db8b4ec99c2a0f45682219
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-06-10 23:12:15 +0900

    Feat : 0610 실습 내용 commit

---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,51 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Slf4j
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @GetMapping("/signup")
+    public String newMemberForm() {
+        return "members/join";
+    }
+
+    @PostMapping("/join")
+    public String createMember(MemberForm form) {
+        Member member = form.toEntity();
+        log.info(member.toString());
+        Member saved = memberRepository.save(member);
+        log.info(saved.toString());
+        return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        Iterable<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members/index";
+    }
+
+}
